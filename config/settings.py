e9bd0af0611141f31caea29e13fa391207563954
Moncka20 <moncayothomas@gmail.com>
2026-09-04 14:48:26 -0500

    Eliminacion de sqlite

--- config/settings.py
+++ config/settings.py
@@ -43,21 +43,15 @@ TEMPLATES = [{
 }]
 WSGI_APPLICATION = 'config.wsgi.application'
 
-if os.getenv('DB_ENGINE', 'sqlite').lower() == 'mysql':
-    DATABASES = {'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': os.getenv('DB_NAME', 'biblioteca_db'),
-        'USER': os.getenv('DB_USER', 'root'),
-        'PASSWORD': os.getenv('DB_PASSWORD', ''),
-        'HOST': os.getenv('DB_HOST', '127.0.0.1'),
-        'PORT': os.getenv('DB_PORT', '3306'),
-        'OPTIONS': {'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"},
-    }}
-else:
-    DATABASES = {'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }}
+DATABASES = {'default': {
+    'ENGINE': 'django.db.backends.mysql',
+    'NAME': os.getenv('DB_NAME', 'biblioteca_db'),
+    'USER': os.getenv('DB_USER', 'root'),
+    'PASSWORD': os.getenv('DB_PASSWORD', ''),
+    'HOST': os.getenv('DB_HOST', '127.0.0.1'),
+    'PORT': os.getenv('DB_PORT', '3306'),
+    'OPTIONS': {'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"},
+}}
 
 AUTH_PASSWORD_VALIDATORS = []
 LANGUAGE_CODE = 'es-co'
